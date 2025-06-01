11997671808d4ee7be0228d42af756f69bf87195
willianss1998 <williannsantos13@gmail.com>
2025-06-01 16:24:37 +0000

    foi atualizado a populacao para melhor leitura do codigo

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -74,7 +74,7 @@ int main() {
     printf("Estado: %c\n", estado1);
     printf("Código: %s\n", codigo1);
     printf("Nome da Cidade: %s\n", nomeCidade1);
-    printf("População: %d milhões de habitantes\n", populacao1);
+    printf("População: %1.f milhões de habitantes\n", populacao1 / 10.000);
     printf("Área: %.2f km²\n", areaKm1);
     printf("PIB: %.2f bilhões de reais\n", pib1);
     printf("Número de Pontos Turísticos: %d\n\n", pontosTuristicos1);
@@ -84,7 +84,7 @@ int main() {
     printf("Estado: %c\n", estado2);
     printf("Código: %s\n", codigo2);
     printf("Nome da Cidade: %s\n", nomeCidade2);
-    printf("População: %d milhoes de habitantes\n", populacao2);
+    printf("População: %.2f milhoes de habitantes\n", populacao2 /10.000);
     printf("Área: %.2f km²\n", areaKm2);
     printf("PIB: %.2f bilhões de reais\n", pib2);
     printf("Número de Pontos Turísticos: %d\n", pontosTuristicos2);
